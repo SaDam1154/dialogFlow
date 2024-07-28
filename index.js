32107f79e9cfeda7a733cc4ef1e2add00cba765d
Sa Đam <20521154@gm.uit.edu.vn>
2024-07-29 00:29:31 +0700

    reDeploy

--- index.js
+++ index.js
@@ -84,3 +84,8 @@ function removeVietnameseTones(stra) {
     str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g, ' ');
     return str;
 }
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
